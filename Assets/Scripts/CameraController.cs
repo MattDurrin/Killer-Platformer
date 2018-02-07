45cecc138ee773a4753a6c0f7f90a3deb1c9e3db
Matt Durrin <matt.durrin@umontana.edu>
2018-02-07 05:04:31 -0700

    Initial file upload

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public Transform target;
+    public Vector3 offset;
+    public bool useOffsetValues;
+    public bool invertMouse;
+    public float rotateSpeed;
+    public Transform pivot;
+    public float maxViewAngle;
+    public float minViewAngle;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (!useOffsetValues)
+        {
+            offset = target.position - transform.position;
+        }
+        pivot.transform.position = target.transform.position;
+        pivot.transform.parent = target.transform;
+
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+
+        //get the mouse x and y axis and rotate
+        float horizontal = Input.GetAxis("Mouse X") * rotateSpeed;
+        float vertical = Input.GetAxis("Mouse Y") * rotateSpeed;
+        target.Rotate(0, horizontal, 0);
+        if (invertMouse)
+        {
+            pivot.Rotate(vertical, 0, 0);
+        }
+        else
+        {
+            pivot.Rotate(-vertical, 0, 0);
+        }
+
+        //Limit camera up and down movement to avoid camera snap
+        if (pivot.rotation.eulerAngles.x > maxViewAngle && pivot.rotation.eulerAngles.x < 180f)
+        {
+            pivot.rotation = Quaternion.Euler(maxViewAngle, 0, 0);
+        }
+
+        if (pivot.rotation.eulerAngles.x > 180f && pivot.rotation.eulerAngles.x < 360f + minViewAngle)
+        {
+            pivot.rotation = Quaternion.Euler(360f + minViewAngle, 0, 0);
+        }
+
+        //Rotate the camera based on the x and y axis postion
+        float desiredYAngle = target.eulerAngles.y;
+        float desiredXAngle = pivot.eulerAngles.x;
+        Quaternion rotation = Quaternion.Euler(desiredXAngle, desiredYAngle, 0);
+        transform.position = target.position - (rotation * offset);
+
+        //Keep the camera from going under the ground
+        if (transform.position.y < target.position.y)
+        {
+            transform.position = new Vector3(transform.position.x, target.position.y - 0.5f, transform.position.z);
+        }
+        transform.LookAt(target);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float moveSpeed;
+    public float jumpForce;
+    public CharacterController theDude;
+    public float gravityScale;
+    private Vector3 moveDirection;
+    // Use this for initialization
+    void Start()
+    {
+        //theDude = GetComponent<Rigidbody>();
+        theDude = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Save the y axis to keep jumping consistent
+        float yStore = moveDirection.y;
+
+        //Move based on the x and y orientation
+        moveDirection = (transform.forward * Input.GetAxis("Vertical") * moveSpeed) + (transform.right * Input.GetAxis("Horizontal") * moveSpeed);
+        moveDirection = moveDirection.normalized * moveSpeed;
+
+        //Reset the y axis
+        moveDirection.y = yStore;
+
+        //Coutneract gravity so falling off of a ledge looks more normal
+        if (theDude.isGrounded)
+        {
+
+            moveDirection.y = 0;
+
+            //Jump when the space bar is pressed
+            if (Input.GetButtonDown("Jump"))
+            {
+                moveDirection.y = jumpForce;
+            }
+
+        }
+
+        //Account for gravity and normalization for fall speed
+        moveDirection.y = moveDirection.y + (gravityScale * Physics.gravity.y * Time.deltaTime);
+
+        //Move the character and normalize for framerate
+        theDude.Move(moveDirection * Time.deltaTime);
+    }
+}
